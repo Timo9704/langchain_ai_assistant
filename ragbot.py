5d4b32ad4d16884a83e1366de89d265998a1e7d3
Timo Schmitz <Timo.schmitz1@web.de>
2024-05-22 11:06:31 +0200

    Clean up and restructure

--- ragbot.py
+++ ragbot.py
@@ -15,6 +15,7 @@ retriever = vectorstore.as_retriever()
 template = """Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
+Think step by step.
 
 {context}
 
@@ -24,7 +25,7 @@ Helpful Answer:"""
 custom_rag_prompt = PromptTemplate.from_template(template)
 
 # LLM
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+llm = ChatOpenAI(model_name="gpt-4o", temperature=0.7)
 
 # Post-processing
 def format_docs(docs):
@@ -37,5 +38,5 @@ rag_chain = (
     | llm
     | StrOutputParser()
 )
-for chunk in rag_chain.stream("Wann tritt eine Bakterienblüte auf?"):
+for chunk in rag_chain.stream("Was ist Aquasabi?"):
     print(chunk, end="", flush=True)
--- vector_embedding/vector_uploader.py
+++ vector_embedding/vector_uploader.py
@@ -1,19 +1,22 @@
+
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Pinecone
 
 load_dotenv()
 
+text_loader_kwargs={'encoding': 'utf-8'}
 loader = DirectoryLoader(
-    "./algen/aquasabi",
-    loader_cls=TextLoader
+    "./algen/",
+    loader_cls=TextLoader,
+    loader_kwargs=text_loader_kwargs
 )
 docs = loader.load()
 
 # Split
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=250, chunk_overlap=50)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 splits = text_splitter.split_documents(docs)
 
 # Embed
