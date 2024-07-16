ebc7d71183f44ef451c4515f95c4c9d49b1df8bf
Timo Schmitz <Timo.schmitz1@web.de>
2024-07-16 14:44:34 +0200

    Add ai_planner with fastApi and Docker

--- ai_planner/ai_planner.py
+++ ai_planner/ai_planner.py
@@ -1,21 +1,24 @@
+import logging
+from fastapi import FastAPI, HTTPException
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Pinecone
-from langchain_community.utilities import GoogleSearchAPIWrapper
+from langchain_google_community import GoogleSearchAPIWrapper
 from dotenv import load_dotenv
 from langchain import hub
-import streamlit as st
 from langchain.agents import AgentExecutor, create_react_agent
-from langchain_community.callbacks.streamlit.streamlit_callback_handler import StreamlitCallbackHandler
 from langchain.chains.llm_math.base import LLMMathChain
-from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import Tool
 from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
-st.set_page_config(
-    page_title="Aquarium Assistant", layout="wide", initial_sidebar_state="collapsed"
-)
+# Logging config
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('uvicorn.error')
+logger.setLevel(logging.INFO)
+
+# FastAPI config
+app = FastAPI()
 
 llm = ChatOpenAI(model="gpt-4o", temperature=0.7, streaming=True)
 
@@ -59,45 +62,34 @@ def tool_google_search():
     return google_search_tool
 
 
-def init_action():
-    tools = [
-        tool_math_calculator(),
-        tool_retriever_vectorstore(),
-        tool_google_search()
-    ]
+@app.post("/planner/")
+async def chat():
+        try:
+            tools = [
+                tool_math_calculator(),
+                tool_retriever_vectorstore(),
+                tool_google_search()
+            ]
 
-    promptTemplate = PromptTemplate.from_template(
-        template="""
-    Du bist ein Aquarium-Experte und berätst einen Anfänger bei der Wahl seines ersten Aquariums, der Technik, des Besatzes und der Bepflanzung.
-    Gehe Schritt für Schritt vor.
-    1. Suche ein Aquarium, mit einer Kantenlänge von maximal 100cm. Der Anfänger wünscht sich ein Set-Aquarium. Das Set darf maximal 500 Euro kosten.
-    2. Plane weitere Technik passend zu dem von dir ausgesuchten Aquarium. Wenn es ein Set-Aquarium mit Filter, Beleuchtung und Heizer ist, dannbeachte, dass die von dir ausgesuchte Technik sich nicht überschneidet.
-    3. Suche Besatz, welcher zur Größe des Aquariums passt. Der Besatz muss auch untereinander vergesellschaftet werden. Denke daran, dass die Person ein Anfänger ist.
-    4. Suche eine passende Bepflanzung aus. Die Bepflanzung richtet sich auch nach den Bedürfnissen der von dir ausgesuchten Tiere und der zur Verfügung stehenden Technik, wie z.B. CO2-Versorgung.
-    5. Bereite deine Planung in einer Tabelle strukturiert auf. Hinterlege wenn möglich einen Link zu einem Online-Shop, bei dem das Produkt gekauft werden kann.
-    """
-    )
+            promptTemplate = PromptTemplate.from_template(
+                template="""
+            Du bist ein Aquarium-Experte und berätst einen Anfänger bei der Wahl seines ersten Aquariums, der Technik, des Besatzes und der Bepflanzung.
+            Gehe Schritt für Schritt vor.
+            1. Suche ein Aquarium, mit einer Kantenlänge von maximal 100cm. Der Anfänger wünscht sich ein Set-Aquarium. Das Set darf maximal 500 Euro kosten.
+            2. Plane weitere Technik passend zu dem von dir ausgesuchten Aquarium. Wenn es ein Set-Aquarium mit Filter, Beleuchtung und Heizer ist, dannbeachte, dass die von dir ausgesuchte Technik sich nicht überschneidet.
+            3. Suche Besatz, welcher zur Größe des Aquariums passt. Der Besatz muss auch untereinander vergesellschaftet werden. Denke daran, dass die Person ein Anfänger ist.
+            4. Suche eine passende Bepflanzung aus. Die Bepflanzung richtet sich auch nach den Bedürfnissen der von dir ausgesuchten Tiere und der zur Verfügung stehenden Technik, wie z.B. CO2-Versorgung.
+            5. Bereite deine Planung in einer Tabelle strukturiert auf. Hinterlege wenn möglich einen Link zu einem Online-Shop, bei dem das Produkt gekauft werden kann.
+            """
+            )
 
-    prompt = hub.pull("hwchase17/react")
-    print(prompt)
-    react_agent = create_react_agent(llm, tools, prompt)
-    agent_executor = AgentExecutor(agent=react_agent, tools=tools, verbose=True)
+            prompt = hub.pull("hwchase17/react")
+            print(prompt)
+            react_agent = create_react_agent(llm, tools, prompt)
+            agent_executor = AgentExecutor(agent=react_agent, tools=tools, verbose=True)
+            answer = agent_executor.invoke({"input": promptTemplate + " Bitte antworte in Deutsch!"})
+            return {"answer": answer}
+        except Exception as e:
+            logger.error(f"Error: {str(e)}")
+            raise HTTPException(status_code=500, detail=str(e))
 
-    with st.form(key="form"):
-        user_input = st.text_input("Stelle deine Frage:")
-        submit_clicked = st.form_submit_button("Stelle Frage")
-
-    output_container = st.empty()
-    if submit_clicked:
-        output_container = output_container.container()
-        output_container.chat_message("user").write(user_input)
-
-        answer_container = output_container.chat_message("assistant")
-        st_callback = StreamlitCallbackHandler(answer_container)
-        cfg = RunnableConfig()
-        cfg["callbacks"] = [st_callback]
-        answer = agent_executor.invoke({"input": promptTemplate + " Bitte antworte in Deutsch!"}, cfg)
-        answer_container.write(answer["output"])
-
-
-init_action()
