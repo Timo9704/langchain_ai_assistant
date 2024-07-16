cb815bd25876cdb76b37aef81d2994c455754214
Timo Schmitz <Timo.schmitz1@web.de>
2024-07-16 14:33:56 +0200

    Add memory to assistant rag bot

--- ai_assistant/ai_assistant.py
+++ ai_assistant/ai_assistant.py
@@ -1,49 +1,127 @@
+import uuid
 import logging
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
-from langchain import hub
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.vectorstores import Pinecone
+from langchain_community.chat_message_histories import ChatMessageHistory
 
 load_dotenv()
 
+# Logging config
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('uvicorn.error')
 logger.setLevel(logging.INFO)
 
+# FastAPI config
 app = FastAPI()
 
+# LangChain config
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 vectorstore = Pinecone.from_existing_index("aquabot", embedding=OpenAIEmbeddings())
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
+
+store = {}
+
+
+# Helper functions
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 
+def generate_session_id():
+    return f"{uuid.uuid4()}"
+
+
 class RequestData(BaseModel):
-    question: str
+    human_input: str
+    session_id: str = Field(default_factory=generate_session_id)
 
 
 @app.post("/assistant/")
 async def chat(request: RequestData):
-    logger.info(f"Received question: {request.question}")
     try:
-        rag_chain = (
-                {"context": retriever | format_docs, "question": RunnablePassthrough()}
-                | prompt
-                | llm
-                | StrOutputParser()
+        system_prompt = (
+            "You are an assistant for question-answering tasks. "
+            "Use the following pieces of retrieved context to answer "
+            "the question. If you don't know the answer, say that you "
+            "don't know. Use three sentences maximum and keep the "
+            "answer concise."
+            "\n\n"
+            "{context}"
         )
-        answer = rag_chain.invoke(request.question)
-        logger.info(f"Answer: {answer}")
-        return {"answer": answer}
+
+        contextualize_q_system_prompt = (
+            "Given a chat history and the latest user question "
+            "which might reference context in the chat history, "
+            "formulate a standalone question which can be understood "
+            "without the chat history. Do NOT answer the question, "
+            "just reformulate it if needed and otherwise return it as is."
+        )
+
+        contextualize_q_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", contextualize_q_system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+
+        history_aware_retriever = create_history_aware_retriever(
+            llm, retriever, contextualize_q_prompt
+        )
+
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+
+        question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+        rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+        conversational_rag_chain = RunnableWithMessageHistory(
+            rag_chain,
+            get_session_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+            output_messages_key="answer",
+        )
+
+        answer = conversational_rag_chain.invoke(
+            {"input": request.human_input},
+            config={
+                "configurable": {"session_id": request.session_id}
+            },
+        )["answer"]
+        return {"answer": answer, "session_id": request.session_id}
+    except Exception as e:
+        logger.error(f"Error: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.delete("/assistant/{session_id}")
+def chat(session_id: str):
+    try:
+        store[session_id].clear()
+        return HTTPException(status_code=200, detail="Session cleared")
     except Exception as e:
         logger.error(f"Error: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
