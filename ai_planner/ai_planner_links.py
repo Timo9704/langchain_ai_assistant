2d2d0eb3c64c40497743524a67f612f3129d24ee
Timo Schmitz <Timo.schmitz1@web.de>
2024-07-29 13:47:04 +0200

    Adjust links code to work without ai

--- ai_planner/ai_planner_links.py
+++ ai_planner/ai_planner_links.py
@@ -1,15 +1,10 @@
-import asyncio
-import logging
+from multiprocessing import Pool
+from fastapi import FastAPI
 import os
-
-from fastapi import FastAPI, HTTPException
+import logging
+from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_google_community import GoogleSearchAPIWrapper
-from dotenv import load_dotenv
-from langchain import hub
-from langchain.agents import AgentExecutor, create_react_agent
-from langchain_core.tools import Tool
-from langchain_core.prompts import PromptTemplate
 
 from model.output_model import PlanningDataLink
 from model.input_model import PlanningDataNoLink, AquariumDataNoLink, FishDataNoLink, PlantDataNoLink
@@ -27,87 +22,47 @@ app = FastAPI()
 # LLM config
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
-# ReAct config
-react_prompt = hub.pull("hwchase17/react")
-
-search_aquarium = GoogleSearchAPIWrapper(google_cse_id=os.environ.get("GOOGLE_CSE_ID_AQUARIUM"))
-search_plants = GoogleSearchAPIWrapper(google_cse_id=os.environ.get("GOOGLE_CSE_ID_PLANTS"))
-
-
-def top2_results_aquarium(query):
-    results = search_aquarium.results(query, 2)
-    return results
-
-
-def top2_results_plants(query):
-    results = search_plants.results(query, 2)
-    return results
-
 
 async def search_links_controller(request: PlanningDataNoLink):
-    result1_task = asyncio.create_task(search_links(request.aquarium))
-    result2_task = asyncio.create_task(search_links(request.fishes))
-    result3_task = asyncio.create_task(search_links(request.plants))
+    items_to_process = [request.aquarium] + request.fishes + request.plants
 
-    answer1 = await result1_task
-    answer2 = await result2_task
-    answer3 = await result3_task
+    pool = Pool()
+    results = [pool.apply_async(search_links, [item]) for item in items_to_process]
 
-    structured_answer = convert_to_json(answer1, answer2, answer3)
+    answers = [result.get() for result in results]
+    pool.close()
+    pool.join()
+
+    structured_answer = convert_to_json(*answers)
     return structured_answer
 
 
-def convert_to_json(answer1, answer2, answer3):
+def convert_to_json(*answers):
     structured_llm = llm.with_structured_output(PlanningDataLink)
-    structured_answer = structured_llm.invoke(answer1 + " " + answer2 + " " + answer3)
+    structured_answer = structured_llm.invoke(" ".join(answers))
     return structured_answer
 
 
-async def search_links(items):
-    try:
-        tools = [
-            Tool(
-                name="Google Suche für Links zu Aquarien und Fische",
-                description="Eine Websuche, um Links zu Aquarien und Fische zu bekommen.",
-                func=top2_results_aquarium,
-            ),
-            Tool(
-                name="Google Suche für Links zu Pflanzen",
-                description="Eine Websuche, um Links zu Pflanzen zu bekommen.",
-                func=top2_results_plants,
-            ),
-        ]
+def search_links(item):
+    search_wrappers = {
+        AquariumDataNoLink: GoogleSearchAPIWrapper(google_cse_id=os.environ.get("GOOGLE_CSE_ID_AQUARIUM")),
+        FishDataNoLink: GoogleSearchAPIWrapper(google_cse_id=os.environ.get("GOOGLE_CSE_ID_FISHES")),
+        PlantDataNoLink: GoogleSearchAPIWrapper(google_cse_id=os.environ.get("GOOGLE_CSE_ID_PLANTS"))
+    }
 
-        if isinstance(items, list):
-            names = ', '.join(
-                [getattr(item, 'fish_lat_name', None) or getattr(item, 'plant_name', None) for item in items])
+    search_function = search_wrappers[type(item)].results
+
+    name = getattr(item, 'fish_lat_name', None) or getattr(item, 'plant_name', None) or getattr(item, 'aquarium_name',
+                                                                                                "")
+    if name:
+        results = search_function(name, 2)
+        if results:
+            link = results[0]['link']
+            if 'aquasabi' in link:
+                link += '?ref=ak'  # Add referral parameter if the link is from Aquasabi
+            output = f"Name: {name}, Link: {link}"
         else:
-            if isinstance(items, AquariumDataNoLink):
-                names = items.aquarium_name
-            elif isinstance(items, FishDataNoLink):
-                names = items.fish_lat_name
-            elif isinstance(items, PlantDataNoLink):
-                names = items.plant_name
-            else:
-                names = ""
-
-        if names != "":
-            promptTemplate = PromptTemplate.from_template(
-                template=f"""
-                    Suche zu jedem Eintrag nach einem Link und füge ihn hinzu.
-                    Wenn es ein Link zu AquaSabi ist, dann füge '?ref=ak' hinzu.
-                    Die Ausgabe sollte wie folgt aussehen: 'Name: XYZ, Link: https://www.beispiel.de'.
-                    Wenn du keinen Link findest, schreibe 'kein Link gefunden'.
-                    {names}
-                    """,
-            )
-
-            react_agent = create_react_agent(llm, tools, react_prompt)
-            agent_executor = AgentExecutor(agent=react_agent, tools=tools, handle_parsing_errors=True, maxIterations=2)
-            answer = agent_executor.invoke({"input": promptTemplate})["output"]
-            return answer
-        else:
-            return ""
-    except Exception as e:
-        logger.error(f"Error: {str(e)}")
-        raise HTTPException(status_code=500, detail=str(e))
+            output = f"Name: {name}, Link: kein Link gefunden"
+    else:
+        output = ""
+    return output
